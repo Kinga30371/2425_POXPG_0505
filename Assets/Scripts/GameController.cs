c85ee7f0440dd2e7026b9487929d4d3ad0649c85
Kinga30371 <s30371@pjwstk.edu.pl>
2025-05-12 13:44:06 +0200

    Koniec - bulls and cows

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,25 +6,85 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public string Word = "after";
+    public int LivesCount = 10;
     public TextMeshProUGUI MainWord;
     public TextMeshProUGUI OurInput;
-    public TextMeshProUGUI SubmittedInput;
+    public TextMeshProUGUI SuggestionText;
     public TMP_InputField InputField;
 
     // Start is called before the first frame update
     void Start()
     {
         MainWord.text = Word;
+        OurInput.text = $"Zostało prób: {LivesCount}.";
     }
 
     public void OnInputChanged(string input)
     {
-        OurInput.text = input;
+        //OurInput.text = input;
     }
 
-    public void OnSubmittedInput() 
+    public void OnClick() 
     {
-        SubmittedInput.text = OurInput.text;
+        Debug.Log($"Przycisk został kliknięty.");
+        LivesCount = LivesCount - 1;
+        //LivesCount -= 1;
+        //LivesCount--;
+        OurInput.text = $"Zostało prób: {LivesCount}.";
+
+        if(Word == InputField.text)
+        {
+            Debug.Log($"Correct guess");
+            //TODO Change text on canvas
+            return;
+        }
+
+        if(Word.Length != InputField.text.Length)
+        {
+            SuggestionText.text = $"Długość się nie zgadza.";
+            return;
+        }
+
+        int bullsCount = CountBulls();
+        int cowsCount = CountCows();
+
+        SuggestionText.text = $"Bulls: {bullsCount} and Cows: {cowsCount}";
+
+        //TODO Jak kończy się życie nic się nie dzieje
+
+    }
+
+    public int CountBulls() 
+    {
+        int result = 0;
+
+        for (int i = 0; i < Word.Length; i++)
+        {
+           if(Word[i] == InputField.text[i])
+           {
+              result++;
+           }
+              
+        }
+
+        return result;
+    }
+
+    public int CountCows()
+    {
+        int result = 0;
+
+        for (int i = 0; i < Word.Length; i++)
+        {
+            // || (or) conajmniej jedna ze stron jest true ==> true
+            // && (and) warunki po lewej i po prawej powinny być true ==> true, else false
+            if (Word[i] != InputField.text[i] && Word.Contains(InputField.text[i]))
+            {
+                result++;
+            }
+        }
+
+        return result;
     }
 
     // Update is called once per frame
